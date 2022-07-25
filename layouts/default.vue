3647057bef8099c62da0498c206af1272899b18d
yusung <smilejjou@gmail.com>
2022-07-25 17:45:09 +0900

    top button

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,35 @@
   <div id="container">
     <Nav />
     <Nuxt />
+    <button class="scrollTop" :class="{active: activeTopButton}" @click="scrollTop()">
+      TOP
+    </button>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      activeTopButton: false
+    }
+  },
+  beforeMount () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    scrollTop () {
+      const isActive = event.target.className.includes('active')
+      if (isActive) { window.scrollTo({ top: 0, behavior: 'smooth' }) }
+    },
+    handleScroll () {
+      // Your scroll handling here
+      // console.log(window.scrollY)
+      const scrY = window.scrollY
+      this.activeTopButton = scrY > 20
+    }
+  }
 }
 </script>
