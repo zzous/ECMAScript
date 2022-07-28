e57b51ada0e1cdde4780086af671ea3007eeac4c
yusung <smilejjou@gmail.com>
2022-07-28 17:36:25 +0900

    progress bar

---
+++ components/common/ProgressBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="progressBar">
+    <span class="gauge" :style="{ width: `${gauge}%` }" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PROGRESSBAR',
+  data () {
+    return {
+      gauge: 0
+    }
+  },
+  beforeMount () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      const winHeight = window.innerHeight
+      const conHeight = document.querySelector('body').offsetHeight
+      const scollY = window.scrollY
+      // console.log('# conHeight', conHeight, '# winHeight', winHeight, '# scrollY', scollY, '# cal', (scollY / (conHeight - winHeight)) * 100)
+      this.gauge = (scollY / (conHeight - winHeight)) * 100
+    }
+  }
+
+}
+</script>
--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="container">
+    <CommonProgressBar />
     <!-- <Nav /> -->
     <Category />
     <Nuxt />
